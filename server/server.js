94cce164ec8620fa6f898b1368b27b106921487e
Keenan Johns <keenanjj13@protonmail.com>
2019-10-11 12:33:28 -0500

    Testing communication between servers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors')
 // const db = require('./db');
+
 const app = express();
 const PORT = 3002;
 
+// Middleware
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/test', (req, res) => {
+    console.log(req);
+    res.send('Test: successful');
+});
 
 app.listen(PORT, () => {
     console.log(`Shits up and running on PORT:${PORT}`);
