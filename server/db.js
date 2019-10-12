73a98d97596e4ada367fbf228ff9422954a31853
Keenan Johns <keenanjj13@protonmail.com>
2019-10-12 11:28:21 -0500

    Added a helper function to limit database seeding to only one operation.
    Added a .catch() to server.js for the api route

--- server/db.js
+++ server/db.js
@@ -28,40 +28,57 @@ let jsonData = [
     toys.results
 ];
 
-Promise.all(jsonData.reduce((acc, cur) => {
-    return [...acc, Products.insertMany(cur)];
-}, []))
-    .then((data) => {
-        console.log('...Saved products to database...');
-        // Create reviews
-        // Data is an array with 4 category arrays in it
-        return data.reduce((acc, cur) => {
-            // Spread acc to concat the last acc value
-            // Spread the cur to flatten the array
-            return [...acc, ...cur.reduce((acc, cur) => {
-                // Create 4 - 6 reviews per product
-                for (let i = 0; i < ~~((Math.random() * (6 - 4)) + 4 + 1); i++) {
-                    // Add each review to acc
-                    acc = [...acc, {
-                        review_id: Number(`${cur.listing_id}${i}`),
-                        date: faker.date.past(45),
-                        description: faker.lorem.sentences(),
-                        rating: Math.floor((Math.random() * 6)),
-                        user_name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-                        user_photo_url: faker.image.avatar(),
-                        product_id: cur.listing_id,
-                        product_user_image_url: 'FIND A ONE THINGY PICTURE ITEM BOI',
-                    }];
-                };
-                return acc
-            }, [])]
-        }, []);
-    })
-    .then((reviews) => {
-        Reviews.insertMany(reviews)
-            .then(() => console.log('...Saved reviews to database...'))
-    })
-    .catch(err => console.log);
+let seedDatabase = function () {
+    Promise.all(jsonData.reduce((acc, cur) => {
+        return [...acc, Products.insertMany(cur)];
+    }, []))
+        .then((data) => {
+            console.log('...Saved products to database...');
+            // Create reviews
+            // Data is an array with 4 category arrays in it
+            return data.reduce((acc, cur) => {
+                // Spread acc to concat the last acc value
+                // Spread the cur to flatten the array
+                return [...acc, ...cur.reduce((acc, cur) => {
+                    // Create 4 - 6 reviews per product
+                    for (let i = 0; i < ~~((Math.random() * (6 - 4)) + 4 + 1); i++) {
+                        // Add each review to acc
+                        acc = [...acc, {
+                            review_id: Number(`${cur.listing_id}${i}`),
+                            date: faker.date.past(45),
+                            description: faker.lorem.sentences(),
+                            rating: Math.floor((Math.random() * 6)),
+                            user_name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+                            user_photo_url: faker.image.avatar(),
+                            product_id: cur.listing_id,
+                            product_user_image_url: 'FIND A ONE THINGY PICTURE ITEM BOI',
+                        }];
+                    };
+                    return acc
+                }, [])]
+            }, []);
+        })
+        .then((reviews) => {
+            Reviews.insertMany(reviews)
+                .then(() => console.log('...Saved reviews to database...'))
+        })
+        .catch(err => console.log);
+}
+
+const seedDatabaseOnce = function (func) {
+    let called = false;
+    return function () {
+        if (!called) {
+            called = true;
+            return func();
+        }
+        return
+    }
+}
+
+seedDatabase = seedDatabaseOnce(seedDatabase);
+
+seedDatabase();
 
 module.exports = {
     Products,
