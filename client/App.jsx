5ee4f136242b71ed844d50d1ed900e99af0e24b4
Keenan Johns <keenanjj13@protonmail.com>
2019-10-26 09:57:18 -0500

    Updated

--- client/App.jsx
+++ client/App.jsx
@@ -31,7 +31,7 @@ class App extends Component {
 
     componentDidMount() {
         window.addEventListener('click', this.updateLocation);
-        this.getProductInfo();
+        this.updateLocation();
     }
 
     getProductInfo(productIdParam) {
